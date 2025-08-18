09719b66b4d006f8d5c861608e9b225a28ac24c2
TheEphir <maksimus247@gmail.com>
2025-08-18 14:58:15 +0300

    made expense models

--- expenses_site/expenses/models.py
+++ expenses_site/expenses/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class ExpenseCategory(models.Model):
+    name = models.CharField(max_length=128, unique=True)
+    description = models.TextField(null=True, blank=True)
+    
+    def __str__(self):
+        return self.name
+    
+
+class Expense(models.Model):
+    ammont = models.DecimalField(decimal_places=2)
+    category = models.ForeignKey(ExpenseCategory, on_delete=models.PROTECT)
+    date = models.DateField()
+    description = models.TextField(null=True, blank=True)
