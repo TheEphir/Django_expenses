0c19ec0c77786bdb568ffb2d798953b1e39ef561
TheEphir <maksimus247@gmail.com>
2025-08-18 13:18:36 +0300

    remade core site, and made expenses app

--- expenses_site/expenses_site/urls.py
+++ expenses_site/expenses_site/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for expenses project.
+URL configuration for expenses_site project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.2/topics/http/urls/
